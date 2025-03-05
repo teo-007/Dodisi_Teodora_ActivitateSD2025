38b404d812edde4935a9050d29de91ea0387883b
teo-007 <dodisiteodora23@stud.ase.ro>
2025-03-05 13:53:09 +0200

    citirea unui nr intreg

--- Seminar01.c
+++ Seminar01.c
@@ -2,7 +2,11 @@
 
 int main()
 {
-	printf("Hello world!");
+	printf("Hello world!\n Introduceti un nr intreg: ");
+
+	int a = 0;
+	scanf_s("%d", &a);
+	printf("You entered: %d", a);
 
 	return 0;
 }
