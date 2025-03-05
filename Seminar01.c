cd72a1a7923890e98a159e05fc692a60328beb03
teo-007 <dodisiteodora23@stud.ase.ro>
2025-03-05 20:54:01 +0200

    recapitulare

--- Seminar01.c
+++ Seminar01.c
@@ -1,15 +1,50 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-int main()
+struct Masina
 {
-	printf("Hello world!\n Introduceti un nr intreg: ");
+	int id;
+	int nrKm;
+	char* model;
+	float pret;
+	char tipMotor;
+};
 
-	int a = 0;
-	scanf_s("%d", &a);
-	printf("You entered: %d", a);
+struct Masina initializare(int id, int nrKm, const char* model, float pret, char tipMotor)
+{
+	struct Masina m;
+	m.id = id;
+	m.nrKm = nrKm;
+	m.pret = pret;
+	m.tipMotor = tipMotor;
+	m.model = (const char*)malloc(strlen(model) * sizeof(const char) + 1);
+	strcpy(m.model, model);
+	return m;
+}
 
-	//citirea unei vb reale
+void afisare(struct Masina m)
+{
+	printf("ID: %d\nNrKm: %d\nModel: %s\nPret: %.2f\nTipMotor: %c\n\n", m.id, m.nrKm, m.model, m.pret, m.tipMotor);
+}
 
+void modifica_Pret(struct Masina* m, float pretNou)
+{
+	if (pretNou > 0);
+	(*m).pret = pretNou;
+}
 
+void dezalocare(struct Masina* m) {
+	free((*m).model);
+	(*m).model = NULL;
+}
+
+int main() {
+	struct Masina masina = initializare(1, 100, "Prosche", 4000.50, 'B'); // 'B'
+	afisare(masina);
+	modifica_Pret(&masina, 3000.50);
+	afisare(masina);
+
+	dezalocare(&masina);
+	afisare(masina);
 	return 0;
 }
